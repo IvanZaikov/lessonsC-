d5be3cc985791e73271d892e682fc4e01d564218
Ivan_Zaikov <van2863@yandex.ru>
2022-08-07 22:55:39 +0500

    seminar1

--- seminars/seminar1/Program.cs
+++ seminars/seminar1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+// Написать программу на вход берет чистло а нв выход дает его квадрат 
+
+Console.Write("input a namber: ");
+int num =Convert.ToInt32(Console.ReadLine()); 
+int quad = num*num;
+Console.WriteLine("The quad of: " + num +  " is " +quad);
+Console.WriteLine($"The quad of: {num} is {quad}");
+*/
+
+/*
+// Задача 2 напишите программу которая принемает два числа , и проверяет явялеться ли первон квадратом второго
+
+
+Console.Write("input a first namber: ");
+int n1 =Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second namber: ");
+int n2 =Convert.ToInt32(Console.ReadLine());
+int quad =n2*n2;
+
+if (n1==quad)
+{
+    Console.WriteLine($"first number {n1} is quad of second namber {n2} ");
+}
+else
+{
+    Console.WriteLine($"first number {n1} is not quad of second namber {n2} ");
+}
+*/
+/*
+//принимает одно число , а на выходе выводит показывает все числа от отрицательного числа до положительного даного числа
+
+Console.Write("input positiv namber: ");
+int number =Convert.ToInt32(Console.ReadLine()); 
+int current = number *(-1); 
+while(current <=number)
+{
+Console.Write(current + " ");
+current++;
+}
+*/
+
+/*
+// напишите программу котора принемает трехзначное число , а на выходе показывает последнее 
+
+Console.Write("input three-digit namber: ");
+int number =Convert.ToInt32(Console.ReadLine());
+int ed=number%10;
+Console.WriteLine($"last number of three-digit namber is {ed}");
+*/
