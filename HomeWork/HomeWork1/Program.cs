2e0467c2f016423d376752affe9d4d8c739db382
Ivan_Zaikov <van2863@yandex.ru>
2022-08-08 14:16:36 +0500

    added infi about HomeWork1

---
+++ HomeWork/HomeWork1/Program.cs
@@ -0,0 +1,65 @@
+﻿
+/*
+// ЗНапишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.Write("input a first namber: ");
+int n1 =Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second namber: ");
+int n2 =Convert.ToInt32(Console.ReadLine());
+
+if (n1>n2 )
+{
+    Console.WriteLine($"first number max {n1} , second namber min {n2} ");
+}
+else
+{
+    Console.WriteLine($"first number min {n1} , second namber max {n2} ");
+}
+
+*/
+/*
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.Write("input a first namber: ");
+int n1 =Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second namber: ");
+int n2 =Convert.ToInt32(Console.ReadLine());
+Console.Write("input a third namber: ");
+int n3 =Convert.ToInt32(Console.ReadLine());
+int max = n1;
+
+if (n2>max)
+max=n2;
+if (n3>max)
+max=n3;
+Console.WriteLine($"the max namber is {max} ");
+
+*/
+
+/*
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("input a namber: ");
+int n1 =Convert.ToInt32(Console.ReadLine());
+
+if (n1%2==0)
+Console.WriteLine($"number {n1} is an even number  ");
+else
+Console.WriteLine($" number {n1} is not an even number");
+*/
+
+/*
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("input a first namber: ");
+int N =Convert.ToInt32(Console.ReadLine());
+
+int current =1; 
+while(current <=N)
+{
+Console.Write(current + " ");
+current++;
+}
+*/
