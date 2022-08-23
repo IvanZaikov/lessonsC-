c1e3801240dcf277ec0b94e81b345a5e2f041926
Ivan_Zaikov <van2863@yandex.ru>
2022-08-24 01:26:51 +0500

    added info about Seminar6

--- seminars/proba/Program.cs
+++ seminars/proba/Program.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
 // // Задача 1. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
 int FindSum (int number)
 {
@@ -156,15 +156,14 @@ Console.WriteLine($" {randomNumber} is {Findbigest(randomNumber)}" );
 */
 
 
-// Задача 4. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+/*
+// Задача 1. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+Найдите сумму отрицательных и положительных элементов массива.
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
 int [] newArray = new int[size];
 for (int i = 0; i < size; i++)
-   { 
-    Console.Write("Input size for array: ");
-    newArray[i] = Convert.ToInt32(Console.ReadLine());
-   }
+    newArray[i] = new Random().Next(minValue, maxValue + 1);
     return newArray;
 }
 void ShowArray(int[] array)
@@ -173,9 +172,145 @@ void ShowArray(int[] array)
         Console.Write(array[i] + " ");
     Console.WriteLine();
 }
-Console.WriteLine();
+void FindMinMaxSum(int[] array)
+{
+    int sumPositive = 0;
+    int sumNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            sumPositive += array[i];
+        else
+            sumNegative += array[i];
+    }
+    Console.WriteLine("Sum of negatives elements is " + sumNegative);
+    Console.WriteLine("Sum of positives elements is " + sumPositive);
+}
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray (a, min, max);
 ShowArray(myArray);
+FindMinMaxSum(myArray);
+*/
 
+/*
+// Задача 2. Напишите программу замены элементов массива: положительные элементы замените на 
+//соответствующие отрицательные, и наоборот.
+
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+int [] newArray = new int[size];
+for (int i = 0; i < size; i++)
+    newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+int [] FindMinMaxSum(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=(array[i]*(-1));
+    }
+    return array;
+}
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray (a, min, max);
+ShowArray(myArray);
+int[] pk=FindMinMaxSum(myArray);
+ShowArray(pk);
+*/
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+int [] newArray = new int[size];
+for (int i = 0; i < size; i++)
+    newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+bool FChekNamber (int[] array, int n)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]==n)return true;  
+    }
+    return false;  
+}
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input namber N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray (a, min, max);
+ShowArray(myArray);
+Console.WriteLine(FChekNamber(myArray,n)); 
+*/
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+int [] newArray = new int[size];
+for (int i = 0; i < size; i++)
+    newArray[i] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+
+  void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+int FindSumOddNumbers (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        sum += array[i];
+    }
+    return sum;
+}
+Console.Write ("Введите длину массива: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите минимальное число массива: ");
+int min = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите максимальное число массива: ");
+int max = Convert.ToInt32 (Console.ReadLine());
+int[] myArray = CreateRandomArray (a, min, max);
+ShowArray(myArray);
+Console.WriteLine ($"Сумма нечетных чисел в массиве = {FindSumOddNumbers (myArray)}");
+*/
+/*
+ 
+for (int i = 2; i <= 10; i++)
+{
+   
     for (int j = 2; j <= 10; j++)
     {
      Console.WriteLine($"{i}*{j}={i*j}");
@@ -188,7 +323,7 @@ ShowArray(myArray);
 // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 //6, 1, 33 -> [6, 1, 33]
-
+/*
 int [,] array (int a,int b, int min, int max )
 {
     int [,] newarray = new int[a,b];
@@ -224,3 +359,67 @@ Console.Write("Введите максимальный элемент масси
 int max=Convert.ToInt32(Console.ReadLine());
 int[,] p=array(a,b,min,max);
 ShowArray(p); 
+*/
+/*
+int [] Creatarray (int size,int min, int max)
+
+{
+    int[] newarray=new int[size];
+    for (int i = 0; i <size; i++)
+    {
+        newarray[i]=new Random().Next(min,max+1);  
+        
+    }
+    return newarray; 
+}
+
+void ShowArra (int [] sharray)
+{
+       for (int i = 0; i < sharray.Length; i++)
+        Console.Write($"{sharray[i]}  " );
+        Console.WriteLine();
+}
+
+  Console.Write("Введите размер: ");
+int size=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите min: ");
+int min=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите max: ");
+int max=Convert.ToInt32(Console.ReadLine());  
+int [] p=Creatarray(size,min,max);
+ShowArra(p);
+ */
+ int[,] Creatarray2 (int a, int b, int vmin, int vmax)
+ {
+    int [,] newarray=new int[a,b];
+    for (int i = 0; i <a; i++)
+    {
+        for (int j = 0; j <b; j++)
+        {
+          newarray[i,j]=new Random().Next(vmin,vmax+1);  
+        }
+    }
+    return newarray;
+ } 
+
+ void ShowArray2 (int[,] array)
+ {
+    for (int i = 0; i <array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" " );
+        }
+        Console.WriteLine();
+    }
+ }
+ Console.Write("Введите колличество строк: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов: ");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите элемент массива min : ");
+int vmin =Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальный элемент массива: ");
+int vmax=Convert.ToInt32(Console.ReadLine());
+int[,] p=Creatarray2(a,b,vmin,vmax);
+ShowArray2(p); 
